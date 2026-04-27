15df99672e73a8e952b07c073e993ed662684294
tuneerbhattacharya <124806733+tuneerbhattacharya@users.noreply.github.com>
2026-04-27 22:05:43 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-def is_safe(query):
-    unsafe = ["drop", "delete", "truncate", "update", "insert", "alter"]
-    return not any(word in query.lower() for word in unsafe)
-
-
-def clean_sql(query):
-    query = query.replace("```sql", "").replace("```", "")
-    query = query.replace("sql\n", "").strip()
-    return query
